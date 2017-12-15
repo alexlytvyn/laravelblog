cc272e82ee4a13e9a222323622d99a694654b1b0
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2017-12-15 19:03:52 +0200

    User Model Tweaks.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+		protected $table = "users";
+		protected $primaryKey = "id";
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -26,4 +30,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+		protected $casts = [
+			'id' => 'integer',
+			'email' => 'string',
+			'password' => 'string',
+			'isAdmin' => 'boolean',
+			'remember_token' => 'string',
+			'created_at' => 'datetime',
+			'updated_at' => 'datetime'
+		]
+
+		protected $dates = [
+			'created_at',
+			'updated_at'		
+		]
 }
